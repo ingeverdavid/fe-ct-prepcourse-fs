9c1ae5d458336633c02b3fda2b4683a2fcb76789
IngEverDavid <ingeverdavid@gmail.com>
2026-03-07 21:43:36 -0500

    Update 10.js

--- M05 Fundamentos Javascript/ejercicios/10.js
+++ M05 Fundamentos Javascript/ejercicios/10.js
@@ -1,8 +1,10 @@
-function esFechaValida(fecha) {
   // La función recibe un argumento "fecha".
   // Comprueba si este corresponde a una fecha válida.
   // Si es así, retorna true, sino retorna false.
   // Tu código:
+function esFechaValida(fecha) {
+  // Verifica si la fecha es válida
+  return !isNaN(new Date(fecha));
 }
 
 module.exports = esFechaValida;
