4514f0dbe69cf0bd5c746360bc6b92ba81686f91
IngEverDavid <ingeverdavid@gmail.com>
2026-03-07 21:42:03 -0500

    Update 09.js

--- M05 Fundamentos Javascript/ejercicios/09.js
+++ M05 Fundamentos Javascript/ejercicios/09.js
@@ -1,8 +1,10 @@
-function esNuloOIndefinido(valor) {
   // La función recibe un argumento "valor".
   // Si este valor es null o undefined, retorna true.
   // De lo contrario, retorna false.
   // Tu código:
+function esNuloOIndefinido(valor) {
+  // Retorna true si el valor es null o undefined
+  return valor === null || valor === undefined;
 }
 
 module.exports = esNuloOIndefinido;
